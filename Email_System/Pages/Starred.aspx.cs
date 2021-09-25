f22a10e6ee581f8ab844e1389fbfaf8a2553b13e
Chandresh Gohel <chandreshgohel26@gmail.com>
2021-09-25 20:23:01 +0530

    Initial Pages done

---
+++ Email_System/Pages/Starred.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Email_System.Pages
+{
+    public partial class Starred : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Email_System/Pages/Trash.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Trash.aspx.cs" Inherits="Email_System.Pages.Trash" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    Trash
+</asp:Content>
