0ea7f219f44c656937d3e9aaa01db331ca8bf191
Ronak Padaliya <ronakpadaliya77@gmail.com>
2021-09-30 15:22:03 +0530

    Login Signup Added

---
+++ Email_System/Login.Master.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Email_System
+{
+    public partial class Login : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void signup_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Pages/Inbox");
+        }
+    }
+}
