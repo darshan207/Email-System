966dedf0e3a36bbff37b1f77136c62c9764039c8
Ronak Padaliya <ronakpadaliya77@gmail.com>
2021-10-18 17:58:59 +0530

    login/signup/homepage module

---
+++ Email_System/Migrations/202110180438423_fname.cs
@@ -0,0 +1,20 @@
+﻿namespace Email_System.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fname : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Emails", "UserId", c => c.Int(nullable: false));
+            AddColumn("dbo.Users", "FirstName", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Users", "FirstName");
+            DropColumn("dbo.Emails", "UserId");
+        }
+    }
+}
